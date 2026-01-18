f34bea987c1f5f026901f243e03b1c4e06df4b04
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-18 10:31:15 +0100

    chore(api): use logger middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import auth from "./routes/auth";
 
 const app = new Hono();
 
+app.use(logger());
 app.route("/auth", auth);
 
 export default app;
