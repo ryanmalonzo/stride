49b63157135b9a90147d16ab6ec62996130dc3fe
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-03-07 12:48:43 +0100

    chore(web): clean up files
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import App from "../App";
 
 export const Route = createFileRoute("/")({
-	component: App,
+	component: HomePage,
 });
+
+function HomePage() {
+	return <div />;
+}
