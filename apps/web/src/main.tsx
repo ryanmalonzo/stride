f3ac6d65f84ba558665e0bd44e83d8eb9b13019f
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-03-07 11:58:14 +0100

    style: set up theme
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import "@fontsource-variable/dm-sans";
+import "@fontsource-variable/lora";
 import "./index.css";
 
 const router = createRouter({
