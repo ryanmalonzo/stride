e4db9c6ff98ebc6a89e7b0f1bc7d7de7f916518d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-03-07 16:14:17 +0100

    chore(i18n): initialize react-i18next
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

---
+++ apps/web/src/i18n/index.ts
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import resourcesToBackend from "i18next-resources-to-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+	.use(
+		resourcesToBackend(
+			(language: string) => import(`./locales/${language}.json`),
+		),
+	)
+	.use(initReactI18next)
+	.init({
+		lng: "en",
+		fallbackLng: "en",
+		interpolation: {
+			escapeValue: false, // React already escapes
+		},
+	});
+
+export default i18n;
